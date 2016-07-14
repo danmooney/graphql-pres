7b55be35fd2de0f79d2fc999fbdc4890c04b3cc2
Dan Mooney <dan.mooney@digitalsurgeons.com>
2016-07-14 18:50:31 -0400

    Update database

--- data/database.js
+++ data/database.js
@@ -13,8 +13,39 @@ let users = {
     1: {
         id: 1,
         firstName: 'Dan',
-        lastName: 'Mooney'
-    }
+        lastName: 'Mooney',
+	    interests: [
+		    'reading',
+		    'eating'
+	    ]
+    },
+    2: {
+        id: 2,
+        firstName: 'Adam',
+        lastName: 'Chambers',
+        interests: [
+	        'drinking',
+            'typing'
+        ]
+    },
+    3: {
+        id: 3,
+        firstName: 'Adam',
+        lastName: 'Soffer',
+        interests: [
+            'legos',
+            'swimming'
+        ]
+    },
+	4: {
+		id: 4,
+		firstName: 'Justin',
+		lastName: 'Cook',
+		interests: [
+			'driving',
+			'cooking'
+		]
+	}
 };
 
 export {
